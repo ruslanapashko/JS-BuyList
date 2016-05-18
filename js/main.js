238067b0165451652366a35da076189ae077b134
ruslanapashko <rusjapashko@yahoo.com>
2016-05-19 00:56:37 +0300

    2 practice

---
+++ js/main.js
@@ -0,0 +1,173 @@
+$(function(){
+    var BASKET = $(".basket");
+    var ITEM_TEMPLATE ='<div class="item">'+$(".item").html()+'</div>';
+    var LIST_TEMPLATE = '<div class="li list_need">'+$(".li").html()+'</div>';
+    var LIST = $('.shelf');
+    var AMOUNT_CIRCLE = '<span class="list_amount">1</span>';
+
+    $("#input").find("input").focus();
+
+    $("#add").click(function(){newItem();})
+  
+    $("#input").find(":input").keyup(function(e){
+        if(e.keyCode == 13) {
+            //newItem(); //work
+            //$("#add").click();//work
+            return false;
+        }
+    });
+
+    $(".item").each(function(i, item) {
+        item = $(item); //to have access to all jQuery functions
+        item.find(".de").click(function(){decrement(item);});
+        item.find(".in").click(function(){increment(item);});
+        item.find(".buy").click(function(){bought(item);});
+        item.find(".n_buy").click(function(){notBought(item);});
+        item.find(".close").click(function(){deleteItem(item);});
+        item.find(".name").keyup(function(){changing(item);})
+    });
+
+
+    
+    function updateNode(node, fn) {
+        node.fadeOut(250, function(){
+            fn();
+            node.fadeIn(250);
+        });
+    }
+    
+
+    function newItem() {
+        var title = $("#input").find(":input").val();
+        if (title != "") {
+            var found = false;
+            $(".name_crossed").each(function(i, item) {
+                var name = $(item).text();
+                if (name == title) {
+                    $(this).parent().parent().find(".in").click();
+                    found = true;
+                }
+            });
+            if(!found){
+                    addItem(title);
+                }
+            $("#input").find(":input").val("");
+        }
+        $("#input").find(":input").focus();
+		
+
+    }
+    function bought(node) {
+        node.addClass("bought");
+        node.find(".name").hide();
+        node.find(".but").hide();
+        node.find(".name_crossed").show();
+        node.find(".close").hide();
+        node.find(".n_buy").show();
+        node.find(".buy").hide();
+
+        $(".list_need").each(function(i, item) {
+            var name = $(item).find(".liname").text();
+            if(name == node.find(".name_crossed").text()){
+                var temp = '<div class="li list_done">'+$(item).html()+'</div>';
+                $(item).remove();
+                $(".shelf_done").append(temp);
+            }
+        });
+    }
+    function notBought(node){
+        node.removeClass("bought");
+        node.find(".but").show();
+        node.find(".name").show();
+        node.find(".name_crossed").hide();
+
+        node.find(".amount").css("margin-left", "0px");
+        node.find('.close').show();
+        node.find(".buy").show();
+        node.find(".n_buy").hide();
+
+        $(".list_done").each(function(i, item) {
+            var name = $(item).find(".liname").text();
+            if (name == node.find(".name_crossed").text()) {
+                var temp = '<div class="li list_need">' + $(item).html() + '</div>';
+                $(item).remove();
+                $(".shelf").append(temp);
+            }
+        });
+    }
+    function decrement(node){
+        var old = parseInt($(node).find('.amount').text());
+        if(old>1) old--;
+        if(old==1){
+            node.find(".de").attr("disabled", true);
+        }
+        node.find(".amount").text(old);
+        $(".list_need").find(".liname").each(function(i, item) {
+           var name = $(item).text();
+            if(name == node.find(".name_crossed").text()){
+                $(this).parent().find(".list_amount").text(old);
+            }
+        });
+    }
+    function increment(node){
+        var old = parseInt($(node).find(".amount").text());
+        if(old==1){
+            node.find(".de").removeAttr("disabled");
+        }
+        old++;
+        node.find(".amount").text(old);
+        $(".liname").each(function(i, item) {
+            var name = $(item).text();
+            if(name == node.find(".name_crossed").text()){
+                $(this).parent().find(".list_amount").text(old);
+            }
+        });
+    }
+    function changing(node){
+        var name = node.find(".name_crossed").text();
+        $(".liname").each(function(i, item) {
+            var name = $(item).text();
+            if (name == node.find(".name_crossed").text()) {
+                $(item).text(node.find(".name").val());
+            }
+        });
+        node.find(".name_crossed").text(node.find(".name").val());
+    }
+    function deleteItem(node){
+        node.remove();
+        var name = node.find(".name_crossed").text();
+        $(".liname").each(function(i, item) {
+            var title = $(item).text();
+            if (name == title) {
+                $(item).parent().remove();
+            }
+        });
+    }
+    function addItem(title){
+        var node = $(ITEM_TEMPLATE); //Create new HTML node
+        node.addClass("item");
+        node.find(".name").val(title); //Set product title
+
+        node.find(".name").keyup(function(){changing(node);});
+
+        node.find(".name_crossed").text(title);
+        node.find(".amount").text(1);
+//Changing amount
+        node.find(".de").click(function(){decrement(node);});
+        node.find(".in").click(function(){increment(node);});
+        node.find(".buy").text("Куплено");
+//Bought Action
+        node.find(".buy").click(function(){bought(node); });
+        node.find(".n_buy").click(function(){ notBought(node); });
+//Delete Action
+        node.find(".close").click(function(){deleteItem(node);});
+        var linode = $(LIST_TEMPLATE);
+        linode.find(".liname").text(title);
+        BASKET.append(node); //Add to the end of the list
+        LIST.append(linode);
+        $("#input").find(":input").focus();
+
+    }
+
+
+});
